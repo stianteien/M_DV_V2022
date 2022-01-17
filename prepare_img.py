20ae327c3685324682cdc7ee29a265a6c9c53d23
Stian Teien <stian.teien@gmail.com>
2022-01-17 11:35:52 +0100

    bilinear for sharpen

--- prepare_img.py
+++ prepare_img.py
@@ -14,6 +14,7 @@ from lib.add_xy_marks import add_xy_marks
 xy = add_xy_marks()
 
 from sklearn.decomposition import PCA
+import datetime
 
 
 # =============================================================================
@@ -49,79 +50,125 @@ roads_mask = roads_mask_30_raw.copy()
 roads_mask[roads_mask == 1] = 2
 combo_mask = roads_mask.copy()
 combo_mask += roof_mask_30_raw
+combo_mask[combo_mask > 2] = 1
 
+
+# =============================================================================
+# Images to use
+# =============================================================================
+
+
+
+def make_train_val_test(dist=[6,2,2]):
+    assert sum(dist) == 10, "sum of dist != 10 (100%)"
+    
+    
+    # Make train data
+    print(f"{datetime.datetime.now()} - Starts making train set")
+    make_set(fnameX="E:/M-DV-STeien/august2019/04/temp_data/X_data.npy",
+             fnamey="E:/M-DV-STeien/august2019/04/temp_data/y_data.npy",
+             cutoff1=0,
+             cutoff2=dist[0],
+             n=1000)
+    
+    
+    # Make val data
+    print(f"{datetime.datetime.now()} - Starts making val set")
+    make_set(fnameX="E:/M-DV-STeien/august2019/04/temp_data/X_data_val.npy",
+             fnamey="E:/M-DV-STeien/august2019/04/temp_data/y_data_val.npy",
+             cutoff1=dist[0],
+             cutoff2=dist[0]+dist[1],
+             n=200)
+    
+    # Make test data
+    print(f"{datetime.datetime.now()} - Starts making test set")
+    make_set(fnameX="E:/M-DV-STeien/august2019/04/temp_data/X_data_test.npy",
+             fnamey="E:/M-DV-STeien/august2019/04/temp_data/y_data_test.npy",
+             cutoff1=dist[0]+dist[1],
+             cutoff2=dist[0]+dist[1]+dist[2],
+             n=500)
+    
 # =============================================================================
 # Cut image in 1/4 for easier memory
 # =============================================================================
-hs_img = img_30_raw
-roof_mask = combo_mask
-nDSM = nDSM_30
-
-cutoff = int(1169/2)
-hs_img = hs_img[:,cutoff:cutoff*2,:] # NB!
-roof_mask = roof_mask[:,cutoff:cutoff*2]
-nDSM = nDSM[:,cutoff:cutoff*2]
-
-# =============================================================================
-# PCA on HS!
-# =============================================================================
-# df_hs = xy.transform_hs(np.array(hs_img), keep_all=True)
-# pca = PCA(n_components=5)
-# hs_pca = pd.DataFrame(pca.fit_transform(df_hs[[i for i in range(176)]]))
-# hs_pca["x"] = df_hs.x
-# hs_pca["y"] = df_hs.y
-
-# hs_img_recreated = np.zeros((hs_img.shape[0], hs_img.shape[1], pca.n_components))
-
-# for i, values in hs_pca.iterrows():
-#     for dim in range(pca.n_components):
-#         hs_img_recreated[int(values.x), int(values.y), dim] = values[dim]
-        
-
-# =============================================================================
-# Add nDSM as a dimension
-# =============================================================================
-hs_pca_ndsm = np.dstack((hs_img, nDSM))
-
-
-# =============================================================================
-# Reduce to RGB  or 100 bands
-# =============================================================================
-img_rgb = np.dstack((hs_img[:,:,76], hs_img[:,:,46], hs_img[:,:,21]))
-img_100bands = hs_img[:,:,:100]
-
-# =============================================================================
-# Combination of images
-# =============================================================================
-img_to_use = hs_pca_ndsm
-
-X_shape = 128
-y_shape = 128
-n = 50
-
-X_r,X_c,X_d = img_to_use.shape
-y_r,y_c = roof_mask.shape
-
-
-X = []
-y = []
-
-r_combinations = [i for i in np.linspace(0, X_r-X_shape, X_r-X_shape+1, dtype=int)]
-c_combinations = [i for i in np.linspace(0, X_c-X_shape, X_c-X_shape+1, dtype=int)]
-
-a,b = (np.random.choice(r_combinations, n), np.random.choice(c_combinations, n))
-c,d = (a+X_shape, b+X_shape)
-
-for a1,b1,c1,d1 in zip(a,b,c,d):
-    X.append(img_to_use[a1:c1, b1:d1].reshape(X_shape,X_shape,X_d)) 
-    y.append(roof_mask[a1:c1, b1:d1].reshape(y_shape,y_shape,1))
+def make_set(fnameX, fnamey, cutoff1, cutoff2, n):
     
-X = np.array(X)
-y = np.array(y)
+    hs_img = img_30_raw
+    roof_mask = roof_mask_30_raw
+    nDSM = nDSM_30
+    
+    cutoff1 = int(cutoff1/10 * hs_img.shape[1])
+    cutoff2 = int(cutoff2/10 * hs_img.shape[1])
+    
+    
+    hs_img = hs_img[:,cutoff1:cutoff2,:] # NB!
+    roof_mask = roof_mask[:,cutoff1:cutoff2]
+    nDSM = nDSM[:,cutoff1:cutoff2]
+    
+    # =============================================================================
+    # PCA on HS!
+    # =============================================================================
+    df_hs = xy.transform_hs(np.array(hs_img), keep_all=True)
+    pca = PCA(n_components=5)
+    hs_pca = pd.DataFrame(pca.fit_transform(df_hs[[i for i in range(176)]]))
+    hs_pca["x"] = df_hs.x
+    hs_pca["y"] = df_hs.y
+    
+    hs_img_recreated = np.zeros((hs_img.shape[0], hs_img.shape[1], pca.n_components))
+    
+    for i, values in hs_pca.iterrows():
+        for dim in range(pca.n_components):
+            hs_img_recreated[int(values.x), int(values.y), dim] = values[dim]
+            
+    
+    # =============================================================================
+    # Add nDSM as a dimension
+    # =============================================================================
+    hs_pca_ndsm = np.dstack((hs_img_recreated, nDSM))
+    
+    
+    # =============================================================================
+    # Reduce to RGB  or 100 bands
+    # =============================================================================
+    img_rgb = np.dstack((hs_img[:,:,76], hs_img[:,:,46], hs_img[:,:,21]))
+    #img_100bands = hs_img[:,:,:100]
+    
+    # =============================================================================
+    # Combination of images
+    # =============================================================================
+    img_to_use = hs_pca_ndsm
+    
+    X_shape = 128
+    y_shape = 128
+    
+    X_r,X_c,X_d = img_to_use.shape
+    y_r,y_c = roof_mask.shape
+    
+    
+    X = []
+    y = []
+    
+    r_combinations = [i for i in np.linspace(0, X_r-X_shape, X_r-X_shape+1, dtype=int)]
+    c_combinations = [i for i in np.linspace(0, X_c-X_shape, X_c-X_shape+1, dtype=int)]
+    
+    a,b = (np.random.choice(r_combinations, n), np.random.choice(c_combinations, n))
+    c,d = (a+X_shape, b+X_shape)
+    
+    for a1,b1,c1,d1 in zip(a,b,c,d):
+        X.append(img_to_use[a1:c1, b1:d1].reshape(X_shape,X_shape,X_d)) 
+        y.append(roof_mask[a1:c1, b1:d1].reshape(y_shape,y_shape,1))
+        
+    X = np.array(X)
+    y = np.array(y)
+    
+    # =============================================================================
+    # Save data
+    # =============================================================================
+    np.save(fnameX, X)
+    np.save(fnamey, y)
+
 
 # =============================================================================
-# Save data
+# Acitivate function for files
 # =============================================================================
-np.save("E:/M-DV-STeien/august2019/04/temp_data/X_data_unseen.npy", X)
-np.save("E:/M-DV-STeien/august2019/04/temp_data/y_data_unseen.npy", y)
-
+make_train_val_test()
